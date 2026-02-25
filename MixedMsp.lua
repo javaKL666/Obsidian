8a546778caa8df6d89816c308f287fae41948d87
JackEyeKL <xb6666661979@outlook.com>
2026-02-25 12:22:15 +0800

    Add files via upload

--- MixedMsp.lua
+++ MixedMsp.lua
@@ -190,12 +190,12 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(28, 28, 30),
-        MainColor = Color3.fromRGB(44, 44, 46),
-        AccentColor = Color3.fromRGB(10, 132, 255),
+        BackgroundColor = Color3.fromRGB(15, 15, 15),
+        MainColor = Color3.fromRGB(25, 25, 25),
+        AccentColor = Color3.fromRGB(125, 85, 255),
         AccentDim = Color3.fromRGB(8, 105, 204),
-        OutlineColor = Color3.fromRGB(84, 84, 88),
-        FontColor = Color3.fromRGB(255, 255, 255),
+        OutlineColor = Color3.fromRGB(40, 40, 40),
+        FontColor = Color3.new(1, 1, 1),
         TextDim = Color3.fromRGB(235, 235, 245),
         Font = Font.fromEnum(Enum.Font.GothamMedium),
 
